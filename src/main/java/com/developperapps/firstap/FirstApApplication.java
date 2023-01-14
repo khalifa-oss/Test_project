03ec57a5244c99f89c4afb375c25304765e71705
khalifa <saidikhalifa82@gmail.com>
2023-01-14 15:21:39 +0100

    first commit

---
+++ src/main/java/com/developperapps/firstap/FirstApApplication.java
@@ -0,0 +1,13 @@
+package com.developperapps.firstap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstApApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstApApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developperapps/firstap/FirstApApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developperapps.firstap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
